da5b38037b322faa8804109072ae1a97956842c9
bahbibfati <fatimazohrabahbib@gmail.com>
2024-03-15 01:20:03 +0000

    connectoin

--- pressure.py
+++
@@ -1,9 +0,0 @@
-import random
-import time
-num=random.random()
-print(num)
-while 1:
-    num=round(random.uniform(10,80),2)
-    time.sleep(0.4)
-    print(num)
-    
--- temperature.py
+++
@@ -1,9 +0,0 @@
-import random
-import time
-num=random.random()
-print(num)
-while 1:
-    num=round(random.uniform(100,120),2)
-    time.sleep(0.4)
-    print(num)
-    
